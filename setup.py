d990bcce0e711cc5fd73f7375d9997543617e186
Yumin <yumin.zheng@mai.mcgill.ca>
2023-12-11 13:10:22 -0500

    version 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='scUNAGI',
-    version='0.1',
+    version='0.2',
     
     long_description=open('README.md').read(),
     packages=find_packages(),
