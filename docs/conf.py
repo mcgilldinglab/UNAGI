7b8b3d6f17c6d0c4259216b91bab647e43f8c26c
Yumin Zheng <37805910+Szym29@users.noreply.github.com>
2023-12-29 12:47:57 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -76,3 +76,17 @@ html_css_files = ["readthedocs-custom.css"] # Override some CSS settings
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+extensions = ['myst_parser']
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "html_image",
+    "html_admonition",
+]
+source_suffix = {
+    ".rst": "restructuredtext",
+    ".ipynb": "myst-nb"
+    
+}
